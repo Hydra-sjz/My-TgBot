e7370838789a3d828f49d9badd504288c34dc103
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-08-22 21:35:30 +0530

    Update __init__.py

--- tgbot/__init__.py
+++ tgbot/__init__.py
@@ -20,7 +20,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-
+import os
 import logging
 from os import environ, mkdir, path, sys
 from dotenv import load_dotenv
