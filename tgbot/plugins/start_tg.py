c80e66f178427fd11794bd0c341b69568fad7e4b
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-08-22 14:21:05 +0530

    Create start_tg.py

---
+++ tgbot/plugins/start_tg.py
@@ -0,0 +1,78 @@
+import logging
+
+from tgbot import tgbot as Bot
+
+from pyrogram import filters
+from pyrogram.types import Message, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+
+logger = logging.getLogger(__name__)
+
+@Bot.on_callback_query(filters.regex('^home$'))
+@Bot.on_message(filters.command('start') & filters.private)
+async def start_handler(cient: Bot, message: Message|CallbackQuery):
+    text = (
+        f"Hello {message.from_user.mention},"
+        "Welcome to the 𝐺𝑜𝑗𝑜 𝑆𝑎𝑡𝑜𝑟𝑢 𝕏 | 𝐵𝑜𝑡! "
+        "This is a powerful bot for Telegram.\n\n"
+        "Click help to know how to use me!"
+    )
+
+    buttons = [[
+        InlineKeyboardButton('Channel 📢', url='https://t.me/XBOTS_X'),
+        InlineKeyboardButton('Commands 📚', callback_data='help')
+        InlineKeyboardButton('About 💡', callback_data='abot')
+        ],[
+        InlineKeyboardButton('❌', callback_data='close')
+    ]]
+    if isinstance(message, Message):
+        await message.reply_text(text, reply_markup=InlineKeyboardMarkup(buttons), quote=True)
+    else:
+        await message.message.edit(text, reply_markup=InlineKeyboardMarkup(buttons))
+
+@Bot.on_callback_query(filters.regex('^help$'))
+@Bot.on_message(filters.command('help') & filters.private & filters.incoming)
+async def help_handler(client: Bot, message: Message | CallbackQuery):
+    text = (
+        "**It's very simple to use me! 😉**\n\n"
+        "test."
+    )
+
+    buttons = [[
+        InlineKeyboardButton('Home 🏕', callback_data='home'),
+        InlineKeyboardButton('❌', callback_data='close')
+    ]]
+
+    if isinstance(message, Message):
+        await message.reply_text(text, reply_markup=InlineKeyboardMarkup(buttons), quote=True)
+    else:
+        await message.message.edit(text, reply_markup=InlineKeyboardMarkup(buttons))
+
+@Bot.on_callback_query(filters.regex('^abot$'))
+@Bot.on_message(filters.command('about') & filters.private & filters.incoming)
+async def about(client: Bot, message: Message|CallbackQuery):
+    me = await client.get_me()
+
+    text = (
+        f"**🤖 Bot Name:** {me.mention()}\n\n"
+        "**📝 Language:** [Python 3](https://www.python.org/)\n\n"
+        "**🧰 Framework:** [Pyrogram](https://github.com/pyrogram/pyrogram)\n\n"
+        "**👨‍💻 Developer:** [Anonymous](https://t.me/Ns_AnoNymouS)\n\n"
+        "**📢 Updates Channel:** [NS Bots](https://t.me/NsBotsOfficial)\n\n"
+        "**👥 Support Group:** [AMC Support](https://t.me/amcDevSupport)\n\n"
+        "**🔗 Source Code:** [GitHub Repository](https://github.com/Ns-AnoNymouS/jiosaavn)\n\n"
+    )
+
+    buttons = [[
+        InlineKeyboardButton('Home 🏕', callback_data='home'),
+        InlineKeyboardButton('❌', callback_data='close')
+    ]]
+    if isinstance(message, Message):
+        await message.reply_text(text, reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True, quote=True)
+    else:
+        await message.message.edit(text, reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
+
+@Bot.on_callback_query(filters.regex('^close$'))
+async def close_cb(client: Bot, callback: CallbackQuery):
+    await callback.answer()
+    await callback.message.delete()
+    await callback.message.reply_to_message.delete()
