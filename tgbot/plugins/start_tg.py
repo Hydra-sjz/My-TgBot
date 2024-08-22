959fadf7c7f9d4a21da747a791393ba41aa5422e
VGX.LEO <119828208+Hydra-sjz@users.noreply.github.com>
2024-08-22 19:18:56 +0530

    Update start_tg.py

--- tgbot/plugins/start_tg.py
+++ tgbot/plugins/start_tg.py
@@ -7,6 +7,7 @@ from pyrogram.types import Message, CallbackQuery, InlineKeyboardButton, InlineK
 
 logger = logging.getLogger(__name__)
 
+#=============START_CMD====================
 text_st = (
    "Hello {},"
    "Welcome to the 𝐺𝑜𝑗𝑜 𝑆𝑎𝑡𝑜𝑟𝑢 𝕏 | 𝐵𝑜𝑡! "
@@ -23,10 +24,11 @@ buttons_st = [[
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def start_handler(bot, message):
-    await message.reply_text(
+    await message.reply_photo(
         photo="https://telegra.ph/file/8fd3a9326d3f0ad19e2d8.jpg",
-        text=text_st.format(message.from_user.first_name), 
-        reply_markup=InlineKeyboardMarkup(buttons), 
+        caption=text_st.format(message.from_user.first_name), 
+        reply_markup=InlineKeyboardMarkup(buttons_st), 
+        quote=True,
     )
 @Bot.on_callback_query(filters.regex('^home$'))
 async def st_cb_handler(bot, query):
@@ -35,13 +37,15 @@ async def st_cb_handler(bot, query):
         reply_markup=InlineKeyboardMarkup(buttons_st),
         disable_web_page_preview=True
     )
-    
+
+
+#=============HELP_CMD====================
 text_hp = (
     "**It's very simple to use me! 😉**\n\n"
     "test."
 )
 buttons_hp = [[
-    InlineKeyboardButton('Home 🏕', callback_data='home'),
+    InlineKeyboardButton('⬅️', callback_data='home'),
     InlineKeyboardButton('❌', callback_data='close')
 ]]
 @Bot.on_message(filters.command('help') & filters.private)
@@ -49,6 +53,7 @@ async def hp_handler(bot, message):
     await message.reply_text(
         text=text_hp, 
         reply_markup=InlineKeyboardMarkup(buttons_hp), 
+        quote=True,
     )
 @Bot.on_callback_query(filters.regex('^help$'))
 async def help_handler(bot, query):
@@ -58,32 +63,39 @@ async def help_handler(bot, query):
         disable_web_page_preview=True
    )
 
-
-
+#=============ABOUT_CMD====================
+text_ab = (
+    "🎈 **AbouT Me** 🎈\n\n"
+    "**🤖 Bot Name:**  𝐺𝑜𝑗𝑜 𝑆𝑎𝑡𝑜𝑟𝑢 𝕏 | 𝐵𝑜𝑡!\n"
+    "**📝 Language:** [Python 3](https://www.python.org/)\n"
+    "**🧰 Framework:** [Pyrogram](https://github.com/pyrogram/pyrogram)\n"
+    "**👨‍💻 Developer:** [VGX.LEO](https://t.me/Vignesh_leo)\n"
+    "**📢 Updates Channel:** [X Bots](https://t.me/Xbots_x)\n"
+    "**👥 Support Group:** [X Support](https://t.me/sp)\n"
+)
+buttons_ab = [[
+    InlineKeyboardButton('⬅️', callback_data='home'),
+    InlineKeyboardButton('❌', callback_data='close')
+]]
+@Bot.on_message(filters.command('about') & filters.private)
+async def hp_handler(bot, message):
+    await message.reply_text(
+        text=text_ab, 
+        reply_markup=InlineKeyboardMarkup(buttons_ab), 
+        quote=True,
+    )
 @Bot.on_callback_query(filters.regex('^abot$'))
-@Bot.on_message(filters.command('about') & filters.private & filters.incoming)
-async def about(client: Bot, message: Message | CallbackQuery):
-    me = await client.get_me()
-
-    text = (
-        f"**🤖 Bot Name:** {me.mention()}\n\n"
-        "**📝 Language:** [Python 3](https://www.python.org/)\n\n"
-        "**🧰 Framework:** [Pyrogram](https://github.com/pyrogram/pyrogram)\n\n"
-        "**👨‍💻 Developer:** [Anonymous](https://t.me/Ns_tt)\n\n"
-        "**📢 Updates Channel:** [NS Bots](https://t.me/NOfficial)\n\n"
+async def help_handler(bot, query):
+    await query.message.edit(
+        text=text_ab, 
+        reply_markup=InlineKeyboardMarkup(buttons_ab),
+        disable_web_page_preview=True
     )
 
-    buttons = [[
-        InlineKeyboardButton('Home 🏕', callback_data='home'),
-        InlineKeyboardButton('❌', callback_data='close')
-    ]]
-    if isinstance(message, Message):
-        await message.reply_text(text, reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True, quote=True)
-    else:
-        await message.message.edit(text, reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
+#=================================
 @Bot.on_callback_query(filters.regex('^close$'))
-async def close_cb(client: Bot, callback: CallbackQuery):
+async def close_cb(bot, callback):
     await callback.answer("👋Hey i am Gojo Satoru 𝕏 Bot")
     await callback.message.delete()
     await callback.message.reply_to_message.delete()
